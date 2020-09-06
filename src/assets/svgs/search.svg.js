04fa3afd5b4530bd1f089af3b7157e8fce9c9267
Dominykas Kutka <dominykaskulka@yahoo.com>
2020-09-07 00:21:05 +0300

    Cleans up svgs for React usage

---
+++ src/assets/svgs/search.svg.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchSvg = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
+      <path d="M31.71,30.29l-7.07-7.06a14.1,14.1,0,1,0-1.41,1.41l7.06,7.07a1,1,0,0,0,1.42,0A1,1,0,0,0,31.71,30.29ZM14,26.09A12,12,0,1,1,26.09,14,12.06,12.06,0,0,1,14,26.09Z" />
+    </svg>
+  );
+};
+
+export default SearchSvg;
