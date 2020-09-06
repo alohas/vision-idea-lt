858cfe8c89a1da5710172b09eb400d93ee4bbf38
Dominykas Kutka <dominykaskulka@yahoo.com>
2020-09-07 00:22:34 +0300

    Updates main file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+//Components
+import TopBar from "./components/TopBar";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TopBar />
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
