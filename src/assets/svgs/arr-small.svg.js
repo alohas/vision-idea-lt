b5d5a88f4812dc05362f4d27da44b3553a6e0a8d
Dominykas Kutka <dominykaskulka@yahoo.com>
2020-09-07 12:38:33 +0300

    Cleans up svgs

---
+++ src/assets/svgs/arr-small.svg.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ArrowSmallSvg = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 20" {...props}>
+      <path d="M1,20a1,1,0,0,1-.68-.29,1,1,0,0,1,0-1.42l8-8.29-8-8.29A1,1,0,0,1,.28.29a1,1,0,0,1,1.37,0L11,10,1.65,19.71A1,1,0,0,1,1,20Z" />
+    </svg>
+  );
+};
+
+export default ArrowSmallSvg;
